9251d50fb1890717639d6d28b68b5bf7e0e4bd4f
Gagulo <grzegorzsokalski86@gmail.com>
2020-04-06 19:54:13 +0200

    Init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home Expenses',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Expenses'),
+      ),
+      body: Center(
+        child: Text('xxx'),
+      ),
+    );
+  }
+}
